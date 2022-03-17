b9e66e0f7d967f03304fb292e327766d04a4a48d
tingtt <ting.taku@gmail.com>
2022-03-17 21:20:12 +0900

    [change] jwtのエラー処理 400 -> 401

--- handler_get.go
+++ handler_get.go
@@ -15,7 +15,7 @@ func get(c echo.Context) error {
 	userId, err := jwt.CheckToken(*jwtIssuer, u)
 	if err != nil {
 		c.Logger().Debug(err)
-		return c.JSONPretty(http.StatusNotFound, map[string]string{"message": err.Error()}, "	")
+		return c.JSONPretty(http.StatusUnauthorized, map[string]string{"message": err.Error()}, "	")
 	}
 
 	// Bind query
